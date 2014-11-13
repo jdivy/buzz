115bc807e12a9b3bb4c5c5f5ff3189aaaeadfd25
Jack Ivy <jack@jackivy.com>
2014-11-12 21:29:24 -0500

    Switch router order to allow use of 'next' on GET method.

--- server/api/brew/index.js
+++ server/api/brew/index.js
@@ -7,8 +7,8 @@ var auth = require('../../auth/auth.service');
 var router = express.Router();
 
 router.get('/', controller.index);
-router.get('/:id', controller.show);
 router.get('/next', controller.next);
+router.get('/:id', controller.show);
 router.post('/', controller.create);
 //router.put('/:id', controller.update);
 //router.patch('/:id', controller.update);
